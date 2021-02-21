0eaca918077b086a8a7511c8fa053dd12f79ab74
Herik Patel <67105093+HerikPatel@users.noreply.github.com>
2021-02-21 15:54:43 +0530

    Add files via upload

--- src/CompanyTest.java
+++ src/CompanyTest.java
@@ -6,23 +6,81 @@ public class CompanyTest {
 
     @Test
     public void add() {
+        // Test-Case1 Adding a full-time employee with the role “Director”
         Company company = new Company();
         Date date = new Date("12/12/2012");
-        Profile profile = new Profile("bob", "cs", date);
-        Fulltime management = new Management(profile, 5000.0, 3);
+        Profile profile = new Profile("Bob,Jack", "CS", date);
+        Fulltime management = new Management(profile, 50000.0, 3);
         assertTrue(company.add(management));
+
+        // Test-Case2 Adding a part-time employee
+        date = new Date("12/12/2011");
+        profile = new Profile("Ryan,Howard", "CS", date);
+        Parttime parttime = new Parttime(profile, 30);
+        assertTrue(company.add(parttime));
+
+        // Test-Case3 Adding employee already in the system it should return false
+        date = new Date("12/12/2012");
+        profile = new Profile("Bob,Jack", "CS", date);
+        management = new Management(profile, 50000.0, 3);
+        assertFalse(company.add(management));
+
+        // Test-Case4 Adding a full-time employee with the role “Department Head”
+        date = new Date("1/1/2013");
+        profile = new Profile("Kathy,James", "IT", date);
+        management = new Management(profile, 45000.0, 2);
+        assertTrue(company.add(management));
+
+        // Test-Case5 Adding a full-time employee with the role “Manager”
+        date = new Date("1/1/2013");
+        profile = new Profile("Dwight,Schrute", "ECE", date);
+        management = new Management(profile, 45000.0, 1);
+        assertTrue(company.add(management));
+
+        // Test-Case6 Adding a full-time employee
+        date = new Date("5/2/2014");
+        profile = new Profile("Creed,Bratton", "ECE", date);
+        Fulltime fulltime = new Fulltime(profile, 40000);
+        assertTrue(company.add(fulltime));
     }
 
     @Test
     public void remove() {
+        // Test-Case1 Removing a full-time employee
         Company company = new Company();
-        Date date = new Date("12/12/2012");
-        Profile profile = new Profile("bob", "cs", date);
-        Fulltime management = new Management(profile, 5000.0, 3);
+        Date date = new Date("1/1/2013");
+        Profile profile = new Profile("Kathy,James", "IT", date);
+        Fulltime management = new Management(profile, 45000.0, 2);
         company.add(management);
+
+        profile = new Profile("Kathy,James", "IT", date);
+        Fulltime fulltime = new Fulltime(profile, 0);
+        assertTrue(company.remove(management));
+
+        // Test-Case2 Removing a full-time employee that does not exist it should return false
+        date = new Date("6/5/2015");
+        profile = new Profile("Raj,Patel", "IT", date);
+        management = new Management(profile, 45000.0, 1);
+        company.add(management);
+
+        profile = new Profile("Jack,Shah", "IT", date);
+        fulltime = new Fulltime(profile, 0);
+        assertFalse(company.remove(fulltime));
+
+        // Test-Case3 Removing a part-time employee
+        date = new Date("2/2/2018");
+        profile = new Profile("Ryan,Kapoor", "ECE", date);
+        Parttime parttime = new Parttime(profile, 30);
+        company.add(parttime);
+
+        profile = new Profile("Ryan,Kapoor", "ECE", date);
+        parttime = new Parttime(profile, 0);
+        assertTrue(company.remove(parttime));
+
     }
 
     @Test
     public void setHours() {
+        
     }
 }
